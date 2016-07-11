315e6713348de5eb2aa635a9c2ea135ecc18ea43
Vasili <vasili-l@yit.co.il>
2016-07-11 07:49:26 +0300

    refactor

--- taskWeek01/scala/src/main/scala/Main.scala
+++ taskWeek01/scala/src/main/scala/Main.scala
@@ -4,6 +4,6 @@ import example.Lists
   * Created by devel on 7/10/16.
   */
 object Main extends App{
-  println(Lists.max(List()))
+  println(Lists.max(List(1,2)))
   println(Lists.sum(List(1,3,2)))
 }
